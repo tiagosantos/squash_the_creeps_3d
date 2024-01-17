c998644c2a7b078cf159c312ebcad68ee36cdd42
Carlos Tiago <ontiago@gmail.com>
2024-01-17 10:43:53 -0300

    add animation to player

--- player.gd
+++ player.gd
@@ -25,6 +25,10 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
+
 	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -49,6 +53,7 @@ func _physics_process(delta):
 				break
 	velocity = target_velocity
 	move_and_slide()
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 func die():
 	hit.emit()
