a262f47672f89b59db96070955f427ac775f0848
Carlos Tiago <ontiago@gmail.com>
2024-01-16 22:48:43 -0300

    add music, restart and UI

--- main.gd
+++ main.gd
@@ -11,8 +11,16 @@ func _on_mob_timer_timeout():
 	var player_position = $Player.position
 	mob.initialize(mob_spawn_location.position, player_position)
 	add_child(mob)
-
-
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+func _ready():
+	$UserInterface/Retry.hide()
+
+func _unhandled_inout(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
+	
